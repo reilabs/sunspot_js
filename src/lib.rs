d94088d37e48ef8d9feceff477114443926c2db7
Matthew Klein <96837318+matthew-a-klein@users.noreply.github.com>
2026-05-13 15:01:28 +0100

    Add bench module to library

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,9 @@ mod types;
 #[cfg(target_arch = "wasm32")]
 pub mod wasm;
 
+#[cfg(all(target_arch = "wasm32", feature = "bench"))]
+pub mod bench;
+
 pub use error::Error;
 pub use pedersen_commitments::{BSB22_FOLD_DST, COMMITMENT_DST, FR_BYTES, bsb22_pok, fold};
 pub use types::{CommitmentInfo, GnarkWitness, PedersenProvingKey, ProvingKey, R1CS, SystemType};
